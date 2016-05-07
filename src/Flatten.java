8fd4670f3157f4e1cf652503c4c0b18b6c9c1008
MysteriousDrD <david.fennell.df@gmail.com>
2016-05-07 12:26:50 +0100

    additional tests for core functionality and more edge cases

--- src/Flatten.java
+++ src/Flatten.java
@@ -50,4 +50,21 @@ public class Flatten {
                 new Integer[]{100,1000,10,11,0,1010},
                 flatten( new Object[]{100,1000,10,11,0,1010}) );
     }
+
+    @Test
+    public void is2DArray()
+    {
+        Assert.assertArrayEquals("2D",
+                new Integer[]{1,2,3},
+                flatten ( new Object[]{1, new Object[]{2,3} } ));
+
+    }
+
+    @Test //this covers uneven depths with a mixture of empty arrays and multi level nesting
+    public void isMultipleDepths()
+    {
+        Assert.assertArrayEquals("Uneven depths",
+                new Integer[]{1,2,3},
+                flatten ( new Object[]{1, new Object[]{new Object[]{2, new Object[]{3}}}, new Object[]{}} ));
+    }
 }
